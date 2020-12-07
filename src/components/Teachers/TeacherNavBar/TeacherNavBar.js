d792433faf91206a24697403c76dc9b16f17a1da
Your Github User Name <david.lav.williams@gmail.com>
2020-12-07 11:07:54 -0600

    the teacher nav bar is now rendering

---
+++ src/components/Teachers/TeacherNavBar/TeacherNavBar.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import "./TeacherNavBar.css"
+
+export const TeacherNavBar = (props) => {
+    return (
+        <ul className="navbar">
+            <li className="navbar__item active">
+                <Link className="navbar__link" to="/teachers">My Classes</Link>
+            </li>
+            <li className="navbar__item">
+                <Link className="navbar__link" to="/Supplies">Supplies</Link>
+            </li>
+        </ul>
+    )
+}

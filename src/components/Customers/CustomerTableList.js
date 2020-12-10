fb881a6b33e47694f9969c8b738f8f6a783a8cc0
Your Github User Name <david.lav.williams@gmail.com>
2020-12-10 11:46:06 -0600

    the descriptions are adding together in an array

--- src/components/Customers/CustomerTableList.js
+++ src/components/Customers/CustomerTableList.js
@@ -12,7 +12,7 @@ export const CustomerTableList=()=>{
     useEffect(()=>{
         getUserClasses().then(getClassListSupplyItem)
     },[])
-
+    // this gets all the items into one list
     const addLists=(myClasses, AllLists)=>{
     let ListOfAllMyItems=[]
     const myClassId=myClasses.map(singleClass=>{
@@ -24,30 +24,46 @@ export const CustomerTableList=()=>{
         })
 
     })
+
     console.log("the main list",ListOfAllMyItems)
+    // this adds them together
     let finalArray=[]
     let idCounter=1
      ListOfAllMyItems.map(singleItem=>{
-        
         const foundItem=finalArray.find(oneItem=>oneItem.name===singleItem.supplyItem.name)
         
         if (foundItem){
            const indexSpot=finalArray.indexOf(foundItem)
            const previousItem=finalArray[indexSpot]
+           const descriptionArray=previousItem.descriptions
+           const  descriptionObj={
+                className:singleItem.classList.name,
+                description: singleItem.description
+            }
+            descriptionArray.push(descriptionObj)
+
+
            const newItem=
            {
                id:previousItem.id,
                name:singleItem.supplyItem.name,
-               number:parseInt(previousItem.number)+parseInt(singleItem.number)
-               
+               number:parseInt(previousItem.number)+parseInt(singleItem.number),
+               descriptions:descriptionArray         
            }
            
            finalArray.splice(indexSpot, 1, newItem)
         } else{
+            const descriptionArray=[]
+           const  descriptionObj={
+                className:singleItem.classList.name,
+                description: singleItem.description
+            }
+            descriptionArray.push(descriptionObj)
            const newItem={
                id:idCounter,
                name:singleItem.supplyItem.name,
                number:singleItem.number,
+               descriptions:descriptionArray
            }
            idCounter++
         //    console.log(newItem)
@@ -55,14 +71,14 @@ export const CustomerTableList=()=>{
         }
 
     })
-    // console.log("finalArray", finalArray)
+    console.log("finalArray", finalArray)
     setFinalAddedArray(finalArray)
     }
 
 
     return (
         <>
-        {console.log(finalAddedArray)}
+        {/* {console.log(finalAddedArray)} */}
         <table>
                 <thead>
                     <tr>

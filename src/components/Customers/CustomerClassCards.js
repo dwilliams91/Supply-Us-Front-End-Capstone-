23e8db05ee4157d40f96a61f927034856515cb21
Your Github User Name <david.lav.williams@gmail.com>
2020-12-11 14:08:24 -0600

    for a second

--- src/components/Customers/CustomerClassCards.js
+++ src/components/Customers/CustomerClassCards.js
@@ -1,4 +1,5 @@
 import React, { useContext } from "react"
+import {Link} from "react-router-dom"
 import { UserClassesContext } from "../DataProviders/UserClassesProvider"
 
 
@@ -8,7 +9,15 @@ export const CustomerClassCards=({myClass, props})=>{
     <div className="CustomerClassesCard">
     <p>{myClass.classList.name}</p>  
     <button onClick={()=>deleteUserClasses(myClass.id).then(props.history.push("./customers"))}>Delete</button>
-    <button> Show Individual Class List</button>
+    <button > <Link to={{
+                pathname:`/customers/{myClass.id}`,
+                state:{
+                chosenClassName:myClass.name,
+                chosenClass:myClass
+            }
+                }}>
+            </Link>Show Individual Class List</button>
+    <h2> </h2>
     </div>
     )
 }

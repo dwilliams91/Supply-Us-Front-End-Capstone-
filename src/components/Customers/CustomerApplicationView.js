bb5331cc2ed30c3b45da0bdb6a73fc692fde89d6
Your Github User Name <david.lav.williams@gmail.com>
2020-12-17 10:37:08 -0600

    the customer side works on mobile

--- src/components/Customers/CustomerApplicationView.js
+++ src/components/Customers/CustomerApplicationView.js
@@ -8,25 +8,36 @@ import { UserClassesProvider } from "../DataProviders/UserClassesProvider"
 import { CustomerForm } from "./CustomerForm"
 import { CustomerIndividualClassTableList } from "./CustomerIndividualClass/CustomerIndividualClassTableList"
 import { CustomerTableList } from "./CustomerTableList"
+import {Row, Col} from "react-bootstrap"
+
 export const CustomerApplicationView = () => {
     return (
         <>
         <div className="customerBackground">
         <div className="CustomerContainer">
+            <Row>
+                
             <ClassListSupplyItemProvider>
                 <TeacherProvider>
                     <ClassListProvider>
                         <UserClassesProvider>
-                            
+                        {/* <Col size="1"></Col> */}
+                                <Col lg="4">
                                 <Route exact path="/customers" render={props => <CustomerForm {...props} />} />
-                                <Route exact path="/customers" render={props => <CustomerTableList {...props} />} />
+                                </Col>
 
+                                {/* <Col sm="1"></Col> */}
+
+                                <Col lg="8">
+                                <Route exact path="/customers" render={props => <CustomerTableList {...props} />} />
+                                </Col>
+                            {/* <Col></Col> */}
                             
                         </UserClassesProvider>
                     </ClassListProvider>
                 </TeacherProvider>
             </ClassListSupplyItemProvider>
-
+            </Row>
             <TeacherProvider>
                 <ClassListSupplyItemProvider>
                     <ClassListProvider>
@@ -43,10 +54,12 @@ export const CustomerApplicationView = () => {
                     </ClassListProvider>
                 </ClassListSupplyItemProvider>
             </TeacherProvider>
+            
             </div>
-
+            
             <Route render={props => <Logout {...props} />} />  
-            </div>     
+            </div>   
+
              </>
 
     )
